8a30471a10505686774c383cc3be62de374fc453
Leonid Svyatnyi <leonid.svyatnyi@gmail.com>
2025-01-27 01:27:16 +0200

    - Replaced the end of the paragraph with a space for task 1 dquote> -Changed four dots to space for task 2 dquote> -Fixed double space issue for task 3 dquote> -Calculated how many times letter 'h' appears in text for task 4 dquote> -Calculated number of words with fist capital letter for task 5 dquote> -Find second place of word 'Tom' dquote> -Worked with variable adwntures_of_tom_sawer_senteces for making tasks 7-10 dquote> -Made a comment for print fuction in tasks: # 2, 3, 7

--- lesson_04/homeworks_04.py
+++ lesson_04/homeworks_04.py
@@ -22,47 +22,79 @@ rolling in wealth."""
 """ Дані у строці adwentures_of_tom_sawer розбиті випадковим чином, через помилку.
 треба замінити кінець абзацу на пробіл .replace("\n", " ")"""
 
+one_line_advanture_text = adwentures_of_tom_sawer.replace("\n", " ")
+print(one_line_advanture_text)
+
 # task 02 ==
 """ Замініть .... на пробіл
 """
+advanture_text_without_four_dots = one_line_advanture_text.replace("....", " ")
+# for better visibility and to check correctness of code has been added print fuction
+# print(advanture_text_without_four_dots)
 
 # task 03 ==
 """ Зробіть так, щоб у тексті було не більше одного пробілу між словами.
 """
-
+advanture_one_space_text = " ".join(advanture_text_without_four_dots.split())
+# for better visibility and to check correctness of code has been added print fuction
+# print(advanture_one_space_text)
 
 # task 04
 """ Виведіть, скількі разів у тексті зустрічається літера "h"
 """
-
+count_h = advanture_one_space_text.count("h")
+print(f"Літера 'h' зустрічається {count_h} разів")
 
 # task 05
 """ Виведіть, скільки слів у тексті починається з Великої літери?
 """
-
+words = advanture_one_space_text.split()
+count_capitalize = 0
+for word in words:
+    if word[0].isupper():
+        count_capitalize += 1
+print(f"Кількість слів, що починаються з великої літери: {count_capitalize}")
 
 # task 06
 """ Виведіть позицію, на якій слово Tom зустрічається вдруге
 """
-
+index_Tom = advanture_one_space_text.find("Tom")
+second_index_Tom = advanture_one_space_text.find("Tom", index_Tom+1)
+print(f"Позиція на якій слово Tom зустрічається вдруге {second_index_Tom}")
 
 # task 07
 """ Розділіть змінну adwentures_of_tom_sawer по кінцю речення.
 Збережіть результат у змінній adwentures_of_tom_sawer_sentences
 """
-adwentures_of_tom_sawer_sentences = None
+adwentures_of_tom_sawer_sentences = advanture_one_space_text.split(". ")
+# for better visibility and to check correctness of code has been added print fuction
+# print(adwentures_of_tom_sawer_sentences)
 
 # task 08
 """ Виведіть четверте речення з adwentures_of_tom_sawer_sentences.
 Перетворіть рядок у нижній регістр.
 """
+fourth_sentence = adwentures_of_tom_sawer_sentences[3].lower()
+print(fourth_sentence)
 
 
 # task 09
 """ Перевірте чи починається якесь речення з "By the time".
 """
+text_to_string = '. '.join(adwentures_of_tom_sawer_sentences)
+print(text_to_string)
+index = text_to_string.find("By the time")
+if index != -1:
+    print("Є речення, що починається з 'By the time'")
+else:
+    print("Немає речення, яке починається з 'By the time'")
 
 
 # task 10
 """ Виведіть кількість слів останнього речення з adwentures_of_tom_sawer_sentences.
 """
+last_sentence = adwentures_of_tom_sawer_sentences[-1]
+print(last_sentence)
+last_sentence_split = last_sentence.split()
+last_sentece_count = len(last_sentence_split)
+print("Word count in the last sentence:", last_sentece_count)
